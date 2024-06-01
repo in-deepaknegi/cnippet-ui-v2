1419b82b6447199d5dc5aea9c088be0d67f0b6c2
in-deepaknegi <negideepak.in@gmail.com>
2024-06-01 17:49:55 +0530

    updated resend-api and hero-1-2

--- components/EmailMessage.tsx
+++ components/EmailMessage.tsx
@@ -1,127 +1,3 @@
-// import {
-//     Body,
-//     Container,
-//     Head,
-//     Heading,
-//     Hr,
-//     Html,
-//     Preview,
-//     Tailwind,
-//     Text,
-//     Row,
-//     Column,
-//     Img,
-//     Link
-// } from '@react-email/components';
-// import * as React from 'react';
-
-// type MessageUsEmailProps = {
-//     name: string;
-//     email: string;
-// };
-
-// const MessageUsEmail = ({ name, email }: MessageUsEmailProps) => {
-//     const previewText = `Hi ${name}. Thanks for joining us at ui.cnippet.com!  We're happy to have you in our community of developers and designers who make great websites easily`;
-
-//     return (
-//         <Html>
-//             <Head />
-//             <Preview>{previewText}</Preview>
-//             <Tailwind>
-//                 <Body className='bg-white my-auto mx-auto font-swir'>
-//                     <Container className='my-20 mx-auto max-w-2xl'>
-//                         <Heading className='text-black text-3xl'>
-//                             Hi {name},
-//                         </Heading>
-
-
-//                         <Text className='text-base'>
-//                             Thanks for joining us at ui.cnippet.com!
-//                         </Text>
-//                         <Text className='text-base'>
-//                             We&apos;re happy to have you in our community of developers and designers who make great websites easily. At ui.cnippet.com, we focus on creating excellent website components and templates using React, Next.js, and TailwindCSS.
-//                         </Text>
-
-//                         <Heading className='text-black text-2xl'>
-//                             What&apos;s New?
-//                         </Heading>
-//                         <Text className='text-base'>
-//                             We are delighted to introduce our latest updates:
-//                         </Text>
-//                         <ul className='list-disc ml-3'>
-//                             <li>
-//                                 <Text className='text-base'>
-//                                     <span className='font-semibold'>New Components</span>: Take a look at our newly added buttons, cards, modals, and more. These carefully crafted components are designed not only for easy integration and extensive customization in your projects, but also to provide a seamless and user-friendly experience for your website visitors.
-
-
-//                                 </Text>
-//                                 <Row>
-//                                     <Column className='px-2'>
-//                                         <Link href="https://ui.cnippet.com/components/hero">
-//                                             <Img src="https://ui.cnippet.com/images/components/hero.png" className='rounded-lg' alt="Cat" width="300" height="150" />
-//                                             <Heading className='text-black text-base font-semibold  mt-2'>
-//                                                 Hero Section
-//                                             </Heading>
-//                                         </Link>
-//                                     </Column>
-//                                     <Column className='px-2'>
-//                                         <Link href="https://ui.cnippet.com/components/flyout-menu">
-//                                             <Img src="https://ui.cnippet.com/images/components/flyout.png" className='rounded-lg' alt="Cat" width="300" height="150" />
-//                                             <Heading className='text-black text-base font-semibold mt-2'>
-//                                                 Flyout Menu
-//                                             </Heading>
-//                                         </Link>
-//                                     </Column>
-//                                     <Column className='px-2'>
-//                                         <Link href="https://ui.cnippet.com/components/footer">
-//                                             <Img src="https://ui.cnippet.com/images/components/footer.png" className='rounded-lg' alt="Cat" width="300" height="150" />
-//                                             <Heading className='text-black text-base font-semibold mt-2'>
-//                                                 Footer
-//                                             </Heading>
-//                                         </Link>
-//                                     </Column>
-
-//                                 </Row>
-//                             </li>
-//                             <li>
-//                                 <Text className='text-base'>
-//                                     <span className=' font-semibold'>Templates</span>: In addition to our expansive library of components, our collection now includes new templates meticulously designed for various industries and purposes. Whether you are working on a portfolio, an e-commerce site, or a corporate website, we have everything you need to create a stunning and professional online presence that will captivate your audience and elevate your brand.
-//                                 </Text>
-//                             </li>
-//                         </ul>
-
-//                         <Heading className='text-black text-2xl'>
-//                             Exclusive Offer for You!
-//                         </Heading>
-//                         <Text className='text-base'>
-//                             As a thank you for joining us, we’re happy to give you a 50% discount on our Pro section. You can get premium parts and templates to boost your projects. Just use the code WELCOME50 when you buy to get this deal.
-//                         </Text>
-
-
-//                         <Heading className='text-black text-2xl'>
-//                             Get Started
-//                         </Heading>
-//                         <Text className='text-base'>
-//                             Visit our website to explore all the new components, templates, and features we have to offer. You will find comprehensive and detailed documentation as well as real-life examples to help you make the most of our diverse range of offerings.
-//                         </Text>
-
-
-//                         <Text className='text-sm font-semibold'>
-//                             Best regards, <br />
-//                             The Cnippet Ui Team.
-//                         </Text>
-
-//                     </Container>
-//                 </Body>
-//             </Tailwind>
-//         </Html>
-//     );
-// };
-
-// export default MessageUsEmail;
-
-
-
 import {
     Body,
     Container,
@@ -226,7 +102,7 @@ export const WelcomeEmail = ({ name, email }: WelcomeEmailProps) => (
                                 <Section>
                                     <Row>
                                         <Column>
-                                            <Link href="/">
+                                            <Link href="https://github.com/in-deepaknegi">
                                                 <Img
                                                     src={`https://ui.cnippet.com/images/meta/github.png`}
                                                     width="32"
@@ -237,7 +113,7 @@ export const WelcomeEmail = ({ name, email }: WelcomeEmailProps) => (
                                             </Link>
                                         </Column>
                                         <Column>
-                                            <Link href="/">
+                                            <Link href="https://www.instagram.com/cnippetui/">
                                                 <Img
                                                     src={`https://ui.cnippet.com/images/meta/instagram.png`}
                                                     width="32"
--- ui/text.jsx
+++ ui/text.jsx
@@ -1,221 +1,59 @@
 import React from "react";
-import { ArrowRight } from "lucide-react";
-import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa6";
+import Image from "next/image";
+import Hero1 from "@/public/images/hero/hero4.jpg";
+import Button from "@/components/ui/Button";
 
-const social = [
-    {
-        title: "facebook",
-        href: "#",
-        icon: <FaFacebook className="h-6 w-6" />,
-    },
-    {
-        title: "insta",
-        href: "#",
-        icon: <FaInstagram className="h-6 w-6" />,
-    },
-    {
-        title: "twitter",
-        href: "#",
-        icon: <FaTwitter className="h-6 w-6" />,
-    },
-    {
-        title: "Youtube",
-        href: "#",
-        icon: <FaYoutube className="h-6 w-6" />,
-    },
-];
-
-const links = [
-    {
-        title: "Contacts",
-        sub: [
-            {
-                name: "+91 11111 11111",
-                href: "tel:+91 97566 20538",
-            },
-            {
-                name: "info@travelo.com",
-                href: "mailto:info@travelo.com",
-            },
-            {
-                name: "office@travelo.com",
-                href: "mailto:office@travelo.com",
-            },
-        ],
-    },
-    {
-        title: "Company",
-        sub: [
-            {
-                name: "About us",
-                href: "#",
-            },
-            {
-                name: "Contacts",
-                href: "#",
-            },
-            {
-                name: "Careers",
-                href: "#",
-            },
-        ],
-    },
-    {
-        title: "Resources",
-        sub: [
-            {
-                name: "Press info",
-                href: "#",
-            },
-            {
-                name: "Customers",
-                href: "#",
-            },
-            {
-                name: "Reviews",
-                href: "#",
-            },
-        ],
-    },
-];
-
-const Footer = () => {
+const C2 = () => {
     return (
-        <footer className="relative isolate bg-black/95 font-swir">
-            <div className=" mx-auto max-w-full px-6 md:max-w-[90%] lg:px-8">
-                <div className="flex flex-col border-gray-300 md:flex-row md:border-b">
-                    <div className="flex w-full flex-col gap-2 py-10 pr-8 md:flex-row md:items-center md:justify-between">
-                        <h3 className="font-swim text-4xl text-gray-50 md:w-[45%]">
-                            Leading the way in adventure
-                        </h3>
-                        <div className="flex gap-x-3">
-                            {social.map((item, i) => (
-                                <a
-                                    key={i}
-                                    href="#"
-                                    className="rounded-full bg-white p-3 shadow-lg"
-                                >
-                                    <span className="sr-only">{item.title}</span>
-                                    {item.icon}
+        <section className="relative isolate overflow-hidden bg-white">
+            <div className="mx-auto">
+                <div className="mx-auto space-x-0 space-y-10 md:flex md:space-x-6 md:space-y-0 lg:mx-0 lg:max-w-none">
+                    <div className="mx-auto my-auto w-full max-w-full py-16 pl-16 md:py-0 lg:max-w-2xl lg:flex-shrink">
+                        <div className="hidden sm:mb-8 sm:flex">
+                            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                Announcing our next round of funding.
+                                <a href="#" className="font-semibold text-black">
+                                    <span className="absolute inset-0" aria-hidden="true"></span>{" "}
+                                    Read more <span aria-hidden="true">&rarr;</span>
                                 </a>
-                            ))}
-                        </div>
-                    </div>
-
-                    <div className="flex w-full flex-col justify-between gap-3 border-t border-gray-300 pt-5 md:flex-row md:items-center md:border-l md:border-t-0 md:pl-8 md:pt-0">
-                        <h3 className="font-swim text-4xl text-gray-50 md:w-[40%]">
-                            Join our Newsletter
-                        </h3>
-
-                        <form action="" className="mx-0 md:w-[60%]">
-                            <div className="flex gap-x-2">
-                                <label htmlFor="email-address" className="sr-only">
-                                    Email address
-                                </label>
-                                <input
-                                    id="email-address"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="min-w-0 flex-auto rounded-full border-0 bg-white px-3.5 text-black outline-none ring-1 ring-inset  
-                                ring-gray-200 placeholder:text-sm placeholder:text-gray-400 focus:ring-inset sm:text-base"
-                                    placeholder="Enter your e-mail"
-                                />
-                                <button
-                                    type="submit"
-                                    className="rounded-full bg-white p-2.5 text-black"
-                                >
-                                    <ArrowRight className="h-5 w-5" />
-                                </button>
                             </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div className="my-16 flex flex-col justify-between md:flex-row">
-                    <div className=" w-full">
-                        <a
-                            href="/"
-                            className="mb-4 flex items-center space-x-3 sm:mb-0 rtl:space-x-reverse"
-                        >
-                            <span className="self-center whitespace-nowrap font-swim text-4xl text-white">
-                                Cnippet
-                            </span>
-                        </a>
-                        <p className="mt-3 text-gray-400 md:w-[50%]">
-                            Travel plans are not really an instrument themselves but a
-                            delivery mechanism or strategy for other mostly
+                        </div>
+                        <h1 className="text-5xl font-semibold md:text-6xl">
+                            Data to enrich your online bussiness
+                        </h1>
+                        <p className="mt-6 text-base text-gray-600 lg:text-lg">
+                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                            fugiat aliqua.
                         </p>
-                    </div>
-                    <div className="mt-10 flex w-full max-w-md flex-wrap justify-between gap-10 md:mt-5 md:max-w-none md:gap-x-32">
-                        {links.map((link, i) => (
-                            <div key={i}>
-                                <h3 className="mb-3 font-swim text-base uppercase text-white">
-                                    {link.title}
-                                </h3>
-                                <ul className="flex flex-col flex-wrap space-y-2 text-sm font-medium uppercase text-gray-400">
-                                    {link.sub.map((item, j) => (
-                                        <li key={j}>
-                                            <a
-                                                href={item.href}
-                                                className="hover:text-white hover:underline"
-                                            >
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                <div className="mx-auto max-w-full border-t border-gray-300 py-4">
-                    <div className="flex flex-col justify-between space-y-4 md:flex-row md:space-y-0 ">
-                        <div className="flex gap-x-4">
-                            <span className="block text-xs text-gray-400 sm:text-left md:text-sm">
-                                <a href="#" className="underline hover:text-white">
-                                    Privacy Policy
-                                </a>
-                            </span>
-                            <span className="block text-xs text-gray-400 sm:text-left md:text-sm">
-                                <a href="#" className="underline hover:text-white">
-                                    Terms of Service
-                                </a>
-                            </span>
-                            <span className="block text-xs text-gray-400 sm:text-left md:text-sm">
-                                <a href="#" className="underline hover:text-white">
-                                    Cookies Policy
-                                </a>
-                            </span>
+                        <div className="mt-10 flex gap-x-6">
+                            <a
+                                href="#"
+                                className="max-w-md rounded-md bg-gradient-to-br from-black to-purple-600 px-3.5 py-2.5 text-sm font-semibold tracking-wide text-white shadow-sm"
+                            >
+                                Get started
+                            </a>
+                            <a
+                                href="#"
+                                className="my-auto text-sm font-semibold leading-6 text-gray-900"
+                            >
+                                Read more <span aria-hidden="true">→</span>
+                            </a>
                         </div>
+                    </div>
 
-                        <div>
-                            <span className="block text-sm text-gray-400 sm:text-right">
-                                © 2024
-                                <a href="/" className="hover:underline">
-                                    {" "}
-                                    Travelo™
-                                </a>
-                                . All Rights Reserved.
-                            </span>
-                            <span className="mt-2 block text-xs text-gray-400 sm:text-right">
-                                Designed and Maintained by:
-                                <a
-                                    target="_blank"
-                                    href="https://www.deepaknegi.in/"
-                                    className=" text-white"
-                                >
-                                    Cnippet
-                                </a>
-                            </span>
-                        </div>
+                    <div className="flex w-full justify-end sm:justify-start sm:gap-8 md:h-screen lg:max-w-full lg:gap-6">
+                        <Image
+                            src={Hero1}
+                            alt="hero image 1"
+                            priority={true}
+                            className="aspect-video h-full w-full object-cover object-bottom md:aspect-auto"
+                        />
                     </div>
                 </div>
             </div>
-        </footer>
+        </section>
     );
 };
 
-export default Footer;
+export default C2;
