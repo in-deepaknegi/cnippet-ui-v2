3d52610e20c4f4522a16eaf2ec5ca762e4708900
in-deepaknegi <negideepak.in@gmail.com>
2024-06-15 12:49:40 +0530

    updated ts code header

--- ui/temp.tsx
+++ ui/temp.tsx
@@ -1,22 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
-type bgImageProps = {
-    src: string;
-    alt: string;
+import { FiArrowUpRight } from "react-icons/fi";
+
+type LinkProps = {
+    title: string;
+    url: string;
 };
+
 type Props = {
+    sup: string;
     title: string;
     description: string;
-    bgImage: bgImageProps;
+    link: LinkProps;
 };
 
 const HeaderDefaults: Props = {
-    title: "Website development",
+    sup: " The web is the world's largest library",
+    title: "Website Development",
     description:
         "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.",
-    bgImage: {
-        src: "url('/images/hero/h14.jpg')",
-        alt: "bg image",
+    link: {
+        title: "Let's talk",
+        url: "#",
     },
 };
 
@@ -24,32 +30,35 @@ export type HeaderProps = React.ComponentPropsWithoutRef<"section"> &
     Partial<Props>;
 
 const Header: React.FC<HeaderProps> = (props) => {
-    const { title, description, bgImage } = {
+    const { sup, title, description, link } = {
         ...HeaderDefaults,
         ...props,
     };
 
     return (
-        <section className="py-8 lg:py-16">
-            <div className="mx-auto max-w-full px-4 md:max-w-[95%] lg:px-8">
-                <div
-                    className="relative isolate overflow-hidden rounded-2xl bg-white shadow-xl shadow-gray-400"
-                    style={{
-                        backgroundImage: bgImage.src,
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                    }}
-                >
-                    <div className="absolute z-[-9] h-full w-full bg-gradient-to-t from-black/30 to-black/40"></div>
+        <section className="relative isolate py-8 lg:py-16">
+            <div className=" mx-auto max-w-full px-6 md:max-w-[95%] lg:px-0">
+                <div className="mx-auto max-w-3xl">
+                    <p className="flex flex-col items-center justify-center gap-4 text-center text-lg text-gray-700 md:flex-row">
+                        <span className="h-0.5 w-16 bg-black"></span>
+                        {sup}
+                    </p>
+                    <h1 className="mt-6 text-center text-4xl text-black md:text-6xl">
+                        {title}
+                    </h1>
+                </div>
 
-                    <div className="mx-auto flex flex-col justify-between gap-y-5 px-6 py-8 text-center md:gap-x-10 md:py-20 lg:flex-row lg:px-8 lg:text-left">
-                        <h2 className="text-4xl font-semibold text-white md:text-6xl">
-                            {title}
-                        </h2>
-                        <p className="mt-auto max-w-xl text-center text-base text-gray-100 md:text-right md:text-lg">
-                            {description}
-                        </p>
-                    </div>
+                <div className="mx-auto mt-8 flex max-w-4xl flex-col items-center justify-between gap-6 md:mt-10 md:flex-row">
+                    <p className="text-center text-lg text-gray-700 md:w-[75%] md:text-left md:text-xl">
+                        {description}
+                    </p>
+                    <Link
+                        href={link.url}
+                        className="flex items-center gap-2 rounded-full bg-black px-5 py-2 text-lg uppercase text-white"
+                    >
+                        {link.title}
+                        <FiArrowUpRight className="h-5 w-5" />
+                    </Link>
                 </div>
             </div>
         </section>
